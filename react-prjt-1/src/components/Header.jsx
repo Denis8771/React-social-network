0741ccff0b2222d0df99e13feae7ad9e7b9c93ee
Denis Korotaevsky <huligan8771@gmail.com>
2022-03-08 21:32:17 +0300

    react-social

---
+++ react-prjt-1/src/components/Header.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+const Header = () => {
+    return <header className='header'>
+        <img src='https://www.logolynx.com/images/logolynx/af/af7f0122799c6404fc1c31d3cabd8e63.jpeg'></img>
+    </header>;
+}
+
+export default Header;
---
+++ react-prjt-1/src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+const Profile = () => {
+    return <div className="content">
+
+        <div>
+            <img src="https://www.ferra.ru/thumb/660x0/filters:quality(75)/imgs/2018/11/26/20/2834700/2320e033e0d1e7097b594b757ab3cb5be8900b5c.jpg"></img>
+        </div>
+        <div>
+            ava + description
+        </div>
+        <div>
+            My posts
+        </div>
+        <div>
+            <div>
+                post1
+            </div>
+            <div>
+                post2
+            </div>
+        </div>
+        <div>
+            New post
+        </div>
+    </div>
+}
+
+export default Profile;
