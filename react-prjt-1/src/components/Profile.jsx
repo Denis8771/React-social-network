37a2b699fe72666c37e2edf337368f32ee72d52f
Denis Korotaevsky <huligan8771@gmail.com>
2022-03-11 01:49:22 +0300

    add lesson 14

--- react-prjt-1/src/components/Profile.jsx
+++ react-prjt-1/src/components/Profile.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import './Profile.css';
+import s from './Profile.module.css';
 
 const Profile = () => {
-    return <div className="content">
+    return <div className={s.content}>
 
         <div>
             <img src="https://www.ferra.ru/thumb/660x0/filters:quality(75)/imgs/2018/11/26/20/2834700/2320e033e0d1e7097b594b757ab3cb5be8900b5c.jpg"></img>
@@ -16,11 +16,11 @@ const Profile = () => {
         <div>
             New post
         </div>
-        <div>
-            <div className="item">
+        <div className={s.posts}>
+            <div className={s.item}>
                 post1
             </div>
-            <div className="item">
+            <div className={s.item}>
                 post2
             </div>
         </div>
