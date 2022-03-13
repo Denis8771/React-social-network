952bceb0cea0cd966a0c4f2b57ee7bdd4a79deef
Denis Korotaevsky <huligan8771@gmail.com>
2022-03-13 22:48:05 +0300

    lesson 16

--- react-prjt-1/src/components/Profile/MyPost/MyPosts.jsx
+++ react-prjt-1/src/components/Profile/MyPost/MyPosts.jsx
@@ -2,26 +2,25 @@ import React from "react";
 import s from './MyPosts.module.css';
 import Post from "./Post/Post";
 
-const MyPosts = () => {
-    return <div>
+const MyPosts = (props) => {
+    debugger;
+    return (
+    <div>
         My posts
         <div>
             <textarea></textarea>
             <buttom>Add post</buttom>
         </div>
 
-
         <div className={s.posts}>
-
-            <Post />
-            <Post />
-            <Post />
-            <Post />
-            <Post />
-            <Post />
-
+            <Post message="Hi, how are you?" />
+            <Post message="It's my first post" />
+            <Post message2="like-15" />
+            <Post message2="like-20" />
         </div>
     </div>
+
+    )
 }
 
 export default MyPosts;
--- react-prjt-1/src/components/Profile/MyPost/Post/Post.jsx
+++ react-prjt-1/src/components/Profile/MyPost/Post/Post.jsx
@@ -1,17 +1,24 @@
 import React from "react";
 import s from './Post.module.css';
 
-const Post = () => {
+const Post = (props) => {
+    
+  
+
     return (
         <div className={s.item}>
             <img src="https://focus.ua/static/storage/thumbs/920x465/3/4a/4678f1be-22e728855eac963a5f1b12e68caee4a3.jpg?v=4080_1" />
-            post1
+
+
+            {props.message}
+            {props.message2};
 
             <div>
                 <span>Like</span>
             </div>
-
         </div>
+
+
     )
 }
 
