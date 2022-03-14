241eda151ced35d980da01cd64228afb553d4217
Denis Korotaevsky <huligan8771@gmail.com>
2022-03-14 21:07:51 +0300

    lesso-18

--- react-prjt-1/src/App.js
+++ react-prjt-1/src/App.js
@@ -1,15 +1,22 @@
 import React from "react";
 import './App.css';
+import Dialogs from "./components/Dialogs/Dialogs";
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 
 const App = () => {
   return (
-    <div className="app-wrapper">
-      <Header />
-      <Navbar />
-      <Profile />
+    <div className="app-wrapper">    {/*   компонента        */}
+      <Header />                     {/*   подкомпонента     */}
+      <Navbar />                     {/*   подкомпонента     */}
+      {/* <Profile />                      подкомпонента     */}
+
+      <div className="app-wrapper-content">
+        <Dialogs />
+      </div>
+
+
     </div>);
 }
 
--- react-prjt-1/src/components/Profile/Profile.jsx
+++ react-prjt-1/src/components/Profile/Profile.jsx
@@ -3,7 +3,7 @@ import MyPosts from "./MyPost/MyPosts";
 import s from './Profile.module.css';
 
 const Profile = () => {
-    return <div className={s.content}>
+    return <div>
 
         <div>
             <img src="https://www.ferra.ru/thumb/660x0/filters:quality(75)/imgs/2018/11/26/20/2834700/2320e033e0d1e7097b594b757ab3cb5be8900b5c.jpg"></img>
