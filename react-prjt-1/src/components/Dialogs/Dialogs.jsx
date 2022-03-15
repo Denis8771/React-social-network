863937f7b69d36956df0abc4aa87d15fa509e380
Denis Korotaevsky <huligan8771@gmail.com>
2022-03-15 19:39:18 +0300

    lesson-21

--- react-prjt-1/src/components/Dialogs/Dialogs.jsx
+++ react-prjt-1/src/components/Dialogs/Dialogs.jsx
@@ -2,11 +2,36 @@ import React from "react"
 import s from './Dialogs.module.css';
 
 const Dialogs = (props) => {
-return (
-    <div>
-        Dialogs
-    </div>
-)
+    return (
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    dimon
+                </div>
+                <div className={s.dialog}>
+                    Andrey
+                </div>
+                <div className={s.dialog}>
+                    Andrey2
+                </div>
+                <div className={s.dialog}>
+                    Andrey3
+                </div>
+                <div className={s.dialog}>
+                    Andrey4
+                </div>
+            </div>
+
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How are you?</div>
+                <div className={s.message}>Yo</div>
+
+            </div>
+
+
+        </div>
+    )
 }
 
 export default Dialogs;
