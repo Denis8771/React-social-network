472c403c2fedf6e23d94f2726264767f020489ce
Denis Korotaevsky <huligan8771@gmail.com>
2022-03-23 00:30:37 +0300

    lesson-29-1

--- react-prjt-1/src/index.js
+++ react-prjt-1/src/index.js
@@ -3,32 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import state from './redux/state';
 
-let posts = [
-    { id: 1, message: "Hi, how are you?", likesCount: 0 },
-    { id: 2, message: "It\'s my fir56657st post", likesCount: 100 },
-    { id: 3, message: "It\'s my fir4tt4st post", likesCount: 1050 },
-    { id: 4, message: "It\'s my fgirst post", likesCount: 0 }
-  ];
-
-
-  let dialogs = [
-    { id: 1, name: "Dimon" },
-    { id: 2, name: "Andrew" },
-    { id: 3, name: "Andrew" },
-    { id: 4, name: "Andrew" },
-    { id: 5, name: "Andrew" },
-];
-
-let messages = [
-    { id: 1, message: "hi" },
-    { id: 2, message: "h2i" },
-    { id: 3, message: "hi3" },
-    { id: 4, message: "h4i" },
-    { id: 5, message: "hi5" },
-];
-
-ReactDOM.render(<App posts={posts} dialogs={dialogs} messages={messages} />, document.getElementById('root'));
+ReactDOM.render(<App appState={state} />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
---
+++ react-prjt-1/src/redux/state.js
@@ -0,0 +1,24 @@
+let state = {
+    posts: [
+        { id: 1, message: "Hi, how are you?", likesCount: 0 },
+        { id: 2, message: "It\'s my fir56657st post", likesCount: 100 },
+        { id: 3, message: "It\'s my fir4tt4st post", likesCount: 1050 },
+        { id: 4, message: "It\'s my fgirst post", likesCount: 0 }
+    ],
+    dialogs: [
+        { id: 1, name: "Dimon" },
+        { id: 2, name: "Andrew" },
+        { id: 3, name: "Andrew" },
+        { id: 4, name: "Andrew" },
+        { id: 5, name: "Andrew" }
+    ],
+    messages: [
+        { id: 1, message: "hi" },
+        { id: 2, message: "h2i" },
+        { id: 3, message: "hi3" },
+        { id: 4, message: "h4i" },
+        { id: 5, message: "hi5" }
+    ]
+}
+
+export default state;
