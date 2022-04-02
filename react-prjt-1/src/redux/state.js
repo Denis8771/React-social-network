71903c8f0df3fa4d66c2205bcd36c38899650591
Denis Korotaevsky <huligan8771@gmail.com>
2022-04-02 23:55:30 +0300

    lesson-34

--- react-prjt-1/src/redux/state.js
+++ react-prjt-1/src/redux/state.js
@@ -9,8 +9,8 @@ let state = {
             { id: 2, message: "It\'s my fir56657st post", likesCount: 100 },
             { id: 3, message: "It\'s my fir4tt4st post", likesCount: 1050 },
             { id: 4, message: "It\'s my fgirst post", likesCount: 0 }
-        ]
-
+        ],
+        newPostText: 'it-kamasutra.com'
     },
     dialogPage: {
 
@@ -18,6 +18,7 @@ let state = {
             { id: 1, message: "hi" },
             { id: 2, message: "h2i" },
             { id: 3, message: "hi3" },
+            { id: 4, message: "h4i" },
             { id: 4, message: "h4i" },
             { id: 5, message: "hi5" }
         ],
@@ -27,34 +28,31 @@ let state = {
             { id: 2, name: "Andrew" },
             { id: 3, name: "Andrew" },
             { id: 4, name: "Andrew" },
+            { id: 5, name: "Andrew" },
             { id: 5, name: "Andrew" }
         ]
-
-    },
-
-    sidebarPage: {
-
-        friends: [
-            { id: 1, name: "Dimon" },
-            { id: 2, name: "Andrew" },
-            { id: 3, name: "Lena" }
-        ]
-
 
     }
 }
 
+window.state = state;
 
-export let addPost = (postMessage) => {
+export let addPost = () => {
     let newPost = {
         id: 5,
-        message: postMessage,
-        likesCount: 0
+        message: state.profilePage.newPostText,
+        likesCount: 10
     };
     state.profilePage.posts.push(newPost);
+    state.profilePage.newPostText = '';
     rerenderEntireTree();
 }
 
+
+export let updateNewPostText = (newText) => {
+    state.profilePage.newPostText = newText;
+    rerenderEntireTree(state);
+}
 
 
 
